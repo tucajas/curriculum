d9c8fa14c3dd0c38cb4bc0aee9d7f3fa3557cfc1
tucajas <tucajas@gmail.com>
2024-06-24 14:50:32 -0300

    actualizacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ export class AppComponent {
 
   longText = `Tecnico superior en programacion con experiencia en las áreas de frontend
   (angular, html, css, typescript ) y backend ( django, python, sql ) diseño web, y
-  desarrollador de webapp. Me considero una persona proactiva,
+  desarrollador de web-app. Me considero una persona proactiva,
   organizada, responsable capaz de resolver cualquier problema de manera
   creativa, acostumbrado a la aplicacion de kayzen, iso 9001 gracias a mi basta
   experiencia en industrias metalurgicas.`;
